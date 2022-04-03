af6ec4b844f4f4367033a854476ce759e96c7848
Hand_MK <thflgg133@naver.com>
2022-04-03 18:34:29 +0900

    Solved.ac

--- CLASS 4/1629번 곱셈.py
+++ CLASS 4/1629번 곱셈.py
@@ -1,17 +1,17 @@
 import sys
 
-def Recursive_Power(num,n):
+def divide_and_conquer(num, n):
     if n == 1:
         return num % C
     
     if n % 2 == 0:
-        y = Recursive_Power(num, n//2)
+        y = divide_and_conquer(num, n//2)
         return y * y % C
     
     else:
-        y= Recursive_Power(num, (n-1)//2)
+        y = divide_and_conquer(num, (n-1)//2)
         return y * y * num % C
 
 
 A, B, C = map(int, sys.stdin.readline().split())
-print(Recursive_Power(A,B))
+print(divide_and_conquer(A,B))
--- CLASS 4/9465번 스티커.py
+++ CLASS 4/9465번 스티커.py
@@ -6,7 +6,9 @@ for _ in range(T):
     N = int(sys.stdin.readline())
     sticker = [list(map(int, sys.stdin.readline().split())) for _ in range(2)]
 
-    for i in range(1,N):
+    # 위에서 때는 경우, 아래서 때는 경우를 나눠서 구한다
+    for i in range(1, N): 
+        # i가 1일때는 왼쪽 대각선에 붙어 있는 스티커는 때지지 않으므로 점수에 더한다
         if i == 1:
             sticker[0][i] += sticker[1][i-1]
             sticker[1][i] += sticker[0][i-1]
